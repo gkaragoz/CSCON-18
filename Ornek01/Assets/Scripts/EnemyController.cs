a1759d4992c866bb223b101e25808d254ff8ac3d
gkaragoz <gkaragoz96@gmail.com>
2018-03-03 16:30:32 +0300

    Enemy has little brain for run to targets.

---
+++ Ornek01/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed;
+    public GameObject target;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake() {
+        _rigidbody = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate() {
+        if (target != null)
+            MoveToTarget();
+    }
+
+    private void MoveToTarget() {
+        Vector3 direction = target.transform.position - transform.position;
+        _rigidbody.velocity = direction.normalized * speed;
+    }
+
+}
