4ddcf1a4506890ead628bdff5c972e10a83826fa
gkaragoz <gkaragoz96@gmail.com>
2018-03-03 15:07:15 +0300

    Init project and add 3 different scenes.

---
+++ Ornek01/Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour {
+
+    public float movementSpeed = 10f;
+
+    private void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //Debug.Log("Horizontal values: " + horizontal);
+        //Debug.Log("Vertical valıues: " + vertical);
+
+        transform.position += new Vector3(horizontal * Time.deltaTime, vertical * Time.deltaTime) * movementSpeed;
+    }
+}
---
+++ Ornek01/Assets/Scripts/PhysicsMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovement : MonoBehaviour {
+
+    public float speed;
+
+    private GameObject _whiteCube;
+    private Rigidbody _rigidbody;
+
+    private void Awake() {
+        _whiteCube = GameObject.Find("White_Cube");
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        _rigidbody.velocity = new Vector3(horizontal, 0f, vertical) * speed;
+    }
+
+    private void HideTheCube(GameObject obj) {
+        obj.SetActive(false);
+    }
+
+    private void ShowTheCube(GameObject obj) {
+        obj.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("I came to sensor's range.");
+        HideTheCube(_whiteCube);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        Debug.Log("I'm in the sensor's range.");
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("I'm out of the sensor.");
+        ShowTheCube(_whiteCube);
+    }
+
+}
