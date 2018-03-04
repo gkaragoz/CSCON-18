7bec3129ce8f44f1ef5ffac4b16ad997edc7b8cd
gkaragoz <gkaragoz96@gmail.com>
2018-03-04 13:05:39 +0300

    Create AnimController.cs script and use with endless loop

---
+++ Ornek01/Assets/Scripts/AnimController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour {
+
+    public List<Sprite> sprites = new List<Sprite>();
+
+    public int index = 0;
+
+    public float frameRate = 1f; //Seconds.
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake() {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = sprites[0];
+
+        StartCoroutine(Run());
+    }
+
+    IEnumerator Run() {
+        while (true) {
+            yield return new WaitForSeconds(frameRate);
+            if (index >= sprites.Count - 1) {
+                index = 0;
+            } else {
+                index++;
+            }
+            _spriteRenderer.sprite = sprites[index];
+        }
+    }
+}
