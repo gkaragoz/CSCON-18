6b525096cd4a49f42d5eee39ab07652e5493dbde
gkaragoz <gkaragoz96@gmail.com>
2018-03-04 15:32:11 +0300

    Bug fixes on Projectile firing direction.

--- Ornek01/Assets/Scripts/TopDownPlayerController.cs
+++ Ornek01/Assets/Scripts/TopDownPlayerController.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class TopDownPlayerController : MonoBehaviour {
 
+    public float currentAngle = 0f;
+    public float projectileSpeed = 15f;
+
+    public Transform projectile;
+    public Transform projectile_SP;
+
     public float movementSpeed = 3f;
 
     private void Update() {
         Move();
         LookAtToMousePosition();
+
+        if (Input.GetMouseButton(0)) {
+            Fire();
+        }
     }
 
     public void LookAtToMousePosition() {
         Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        currentAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.AngleAxis(currentAngle, Vector3.forward);
     }
 
     public void Move() {
@@ -23,5 +33,10 @@ public class TopDownPlayerController : MonoBehaviour {
         float vertical = Input.GetAxis("Vertical");
 
         transform.position += new Vector3(horizontal, vertical, 0f) * movementSpeed * Time.deltaTime;
+    }
+
+    public void Fire() {
+        GameObject obj = Instantiate(projectile, projectile_SP.position, Quaternion.identity).gameObject;
+        obj.GetComponent<Rigidbody2D>().velocity = (projectile_SP.position - transform.position) * projectileSpeed;
     }
 }
