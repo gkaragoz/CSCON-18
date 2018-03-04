380263d1724e060c7852e275072fc08d05da21d7
gkaragoz <gkaragoz96@gmail.com>
2018-03-04 12:08:17 +0300

    Create Navmesh system and add an example scene.

---
+++ Ornek01/Assets/EnemyAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+
+    public Transform target;
+
+    private NavMeshAgent _agent;
+
+    private void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        _agent.destination = target.position;
+    }
+
+}
---
+++ Ornek01/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Transform target;
+
+    private NavMeshAgent _agent;
+
+    private void Start() {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        _agent.destination = target.position;
+    }
+
+}
