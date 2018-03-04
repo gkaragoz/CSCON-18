cf4471295f74ffe5e38f5887b0aa6fe8364223a9
gkaragoz <gkaragoz96@gmail.com>
2018-03-04 16:44:58 +0300

    Create Projectile.cs and CameraController.cs

---
+++ Ornek01/Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float deathTime = 3f; //Seconds
+
+    private void Awake() {
+        StartCoroutine(Die());
+    }
+
+    IEnumerator Die() {
+        yield return new WaitForSeconds(deathTime);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Enemy") {
+            Destroy(gameObject);
+        }
+    }
+}
