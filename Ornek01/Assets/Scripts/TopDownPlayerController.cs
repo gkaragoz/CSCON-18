ff51e1dff296167418551534e81abf19a63c78d6
gkaragoz <gkaragoz96@gmail.com>
2018-03-04 14:11:42 +0300

    Add LookAtToMousePosition method.

---
+++ Ornek01/Assets/Scripts/TopDownPlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownPlayerController : MonoBehaviour {
+
+    public float movementSpeed = 3f;
+
+    private void Update() {
+        Move();
+        LookAtToMousePosition();
+    }
+
+    public void LookAtToMousePosition() {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public void Move() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector2(horizontal, vertical) * movementSpeed * Time.deltaTime);
+    }
+}
